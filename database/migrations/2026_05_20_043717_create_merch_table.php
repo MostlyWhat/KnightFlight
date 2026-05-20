457d193032c65837bbe631ab88ff531c13f99ea1
Pongsupa Supapa <mostlywhatstudios@gmail.com>
2026-05-20 10:48:34 -0500

    fix: remove comments

--- database/migrations/2026_05_20_043717_create_merch_table.php
+++ database/migrations/2026_05_20_043717_create_merch_table.php
@@ -4,12 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('customers', function (Blueprint $table) {
-            $table->id();                          // just 'id' — Laravel default
+            $table->id();
             $table->foreignId('user_id')
                 ->constrained()
                 ->cascadeOnDelete();
@@ -19,8 +18,8 @@ return new class extends Migration
 
         Schema::create('addresses', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('customer_id')       // matches 'customers.id' automatically
-            ->constrained()
+            $table->foreignId('customer_id')
+                ->constrained()
                 ->cascadeOnDelete();
             $table->string('address_name');
             $table->string('street1');
